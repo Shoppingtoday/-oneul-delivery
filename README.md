1f7f7457f5211338f249e5d76c6f0d4e65188adf
Shoppingtoday <mslted0630@gmail.com>
2026-06-26 21:06:05 +0900

    V7 update

---
+++ README.md
@@ -0,0 +1,15 @@
+# 바로착 V7
+
+GitHub Desktop 업로드용 완성본입니다.
+
+필수 파일:
+- index.html
+- manifest.json
+- sw.js
+- products.json
+- icon-192.png
+- icon-512.png
+- images 폴더
+
+업로드 후 GitHub Pages 주소를 Safari에서 열고:
+공유 버튼 → 홈 화면에 추가
--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('oneul-v6').then(c=>c.addAll(['./','index.html','manifest.json','icon-192.png','icon-512.png','products.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('barochak-v7-final').then(cache => {
+      return cache.addAll([
+        './',
+        'index.html',
+        'manifest.json',
+        'products.json',
+        'icon-192.png',
+        'icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
